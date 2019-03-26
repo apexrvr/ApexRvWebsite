7b84b7e747314c1ba33d0ee38f7a4095fd34dabc
Ravan klar <ravanklar@Ravans-MBP.hitronhub.home>
2019-03-25 18:50:47 -0700

    update

--- javascript/function.js
+++ javascript/function.js
@@ -1,21 +1,24 @@
 
 // header color change
-$(document).ready(function(){       
-            var scroll_pos = 0;
-            $(document).scroll(function() { 
-                scroll_pos = $(this).scrollTop();
-                if(scroll_pos > 10) {
-                    $(".header1").css('display', 'none');
-                    $(".header2").fadeIn('slow');
-                    $("header").css('background-color', '#5C7AB8');
-                } else {
-                    $(".header1").fadeIn('slow');
-                    $(".header2").fadeOut('slow');
-                    $("header").css('background-color', '#fff');
 
-                }
-            });
-        });
+
+
+$(document).ready(function(){          
+  var scroll_pos = 0;
+    $(document).scroll(function() { 
+        scroll_pos = $(this).scrollTop();
+        if(scroll_pos > 10) {
+            $(".header1").css('display', 'none');
+            $(".header2").fadeIn('slow');
+            $("header").css('background-color', '#5C7AB8');
+        } else {
+            $(".header1").fadeIn('slow');
+            $(".header2").fadeOut('slow');
+            $("header").css('background-color', '#fff');
+
+        }
+    });
+    });
  //comment rgba(0, 0, 255, 0.0000001) ,, rgba(53,54,59,0.925)
     
 // smooth scroll
@@ -44,32 +47,31 @@ $(document).ready(function(){
     });
   });
   
-  $(document).ready(function () {
-    $('div.toshow').fadeIn(4000);
+$(document).ready(function () {
+   // $('div.toshow').fadeIn(4000);
 });
 // fading in elements on scroll
   $(function(){  // $(document).ready shorthand
-    $('.about').fadeIn('slow');
-    $('.boxes').fadeIn('slow');
-    $('.services').fadeIn('slow');
+  //  $('.about').fadeIn('slow');
+  //  $('.boxes').fadeIn('slow');
+  //  $('.services').fadeIn('slow');
   });
   
-  $(document).ready(function() {
-      /* Every time the window is scrolled ... */
-      $(window).scroll( function(){
-          /* Check the location of each desired element */
-          $('.hideme').each( function(i){
-              
-              var bottom_of_object = $(this).position().top + $(this).outerHeight();
-              var bottom_of_window = $(window).scrollTop() + $(window).height();
-              /* If the object is completely visible in the window, fade it it */
-              if( bottom_of_window > bottom_of_object ){
-                  $(this).animate({'opacity':'1'},3000);                      
-              }else{
-              }
-              
-          }); 
-      
-      });
+$(document).ready(function() {
+    /* Every time the window is scrolled ... */
+    // $(window).scroll( function(){
+    //     /* Check the location of each desired element */
+    //     $('.hideme').each( function(i){
+    //         var bottom_of_object = $(this).position().top + $(this).outerHeight();
+    //         var bottom_of_window = $(window).scrollTop() + $(window).height();
+    //         /* If the object is completely visible in the window, fade it it */
+    //         if( bottom_of_window > bottom_of_object ){
+    //             $(this).animate({'opacity':'1'},3000);                      
+    //         }else{
+    //         }
+            
+    //     }); 
+    
+    //});
       
   });
